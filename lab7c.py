d10c62034c1a74006765e2121e14608a2741cc6d
Sangeeth Vethanayagam <svethanayagam3@myseneca.ca>
2025-03-20 20:02:17 -0400

    Added two new functions
    time_to_sec and sec_to_time

--- lab7c.py
+++ lab7c.py
@@ -57,6 +57,19 @@ def change_time(time, seconds):
              
     return None
 
+def time_to_sec(time):
+    '''convert a time object to a single integer representing the number of seconds from mid-night'''
+    minutes = time.hour * 60 + time.minute
+    seconds = minutes * 60 + time.second
+    return seconds
+
+def sec_to_time(seconds):   
+    '''convert a given number of seconds to a time object in hour,minute,second format'''
+    time = Time()
+    minutes, time.second = divmod(seconds, 60)
+    time.hour, time.minute = divmod(minutes,60)
+    return time
+
 def valid_time(t):
     """check for the validity of the time object attributes:
         24 > hour > 0, 60 > minute > 0, 60 > second > 0 """
